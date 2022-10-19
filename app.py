7311888702179f0ef3c268d55703c3ec3fae8895
Vijish Madhavan <53169213+vijishmadhavan@users.noreply.github.com>
2022-10-19 19:46:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ option = st.selectbox('Select your choice of search',
 
 st.write('You selected:', option)
 
-title = st.text_input('Search input based on option', '')
-SongName = st.text_input('Song name/Name you would like to display', '')
+title = st.text_input('Search input based on option', 'dil chahta hai')
+SongName = st.text_input('Song name/Name you would like to display', 'Dil Chahta Hai!')
 
 sp = spotipy.Spotify(
     client_credentials_manager=SpotifyClientCredentials(client_id="e6de42c45f314f1da04a9e63b0cdce5c", client_secret="f957f7dbd7db49de9d9af758941d4d85"))
