23adf68b42e25e83fc3d4938c0241a1ea12553a3
Vijish Madhavan <53169213+vijishmadhavan@users.noreply.github.com>
2022-10-26 22:21:47 +0530

    Update tkapp.py

--- tkapp.py
+++ tkapp.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from code.mn import *
+from code.main import *
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
@@ -8,13 +8,14 @@ from urllib.request import urlopen
 from io import BytesIO
 
 
-uploaded_file = st.file_uploader("Choose a file")
-
 sp = spotipy.Spotify(
     client_credentials_manager=SpotifyClientCredentials(client_id="e6de42c45f314f1da04a9e63b0cdce5c", client_secret="f957f7dbd7db49de9d9af758941d4d85"))    
 
 
-ZipfileDotZip = get_art_with_xls(uploaded_file,sp)
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    ZipfileDotZip = get_art_with_xls(uploaded_file,sp)
 
 with open(ZipfileDotZip, "rb") as f:
     bytes = f.read()
